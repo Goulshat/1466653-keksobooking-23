64c3d734006e7e4a09fb58df6e64ecd7310227ac
Goulshat <76894691+Goulshat@users.noreply.github.com>
2021-07-08 23:12:29 +0500

    Добавляет валидацию формы

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {offersFragment} from './offers.js';
 import {formDisableToggle} from './disable-form.js';
+import './form.js';
 
 const map = document.querySelector('#map-canvas');
 map.appendChild(offersFragment);
